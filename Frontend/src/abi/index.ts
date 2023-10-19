13bdbae7bcecc096b9c03e5410c9baa5399569a7
gg <gracecrypto03@gmail.com>
2023-10-19 20:29:28 +0530

    fix typo

--- Frontend/src/abi/index.ts
+++ Frontend/src/abi/index.ts
@@ -1,6 +1,6 @@
-import BountyAAbi from './BountyAbi.json'
+import BountyAbi from './BountyAbi.json'
 import BountyFactoryAbi from './BountyFactoryAbi.json'
 import Erc20ReputationAbi from './Erc20ReputationAbi.json'
 import Erc721KYHAbi from './Erc721KYHAbi.json'
 
-export { BountyAAbi, BountyFactoryAbi, Erc20ReputationAbi, Erc721KYHAbi }
+export { BountyAbi, BountyFactoryAbi, Erc20ReputationAbi, Erc721KYHAbi }
