a62df4e190b83a44d0526f413d16d561b622dc67
gg <gracecrypto03@gmail.com>
2023-10-19 20:28:52 +0530

    bounty abi

--- Frontend/src/abi/index.ts
+++ Frontend/src/abi/index.ts
@@ -1,9 +1,6 @@
-import BountyFactoryAbi from "./BountyFactoryAbi.json";
-import Erc20ReputationAbi from "./Erc20ReputationAbi.json";
-import Erc721KYHAbi from "./Erc721KYHAbi.json";
+import BountyAAbi from './BountyAbi.json'
+import BountyFactoryAbi from './BountyFactoryAbi.json'
+import Erc20ReputationAbi from './Erc20ReputationAbi.json'
+import Erc721KYHAbi from './Erc721KYHAbi.json'
 
-export {  
-    BountyFactoryAbi,
-    Erc20ReputationAbi,
-    Erc721KYHAbi
-};
+export { BountyAAbi, BountyFactoryAbi, Erc20ReputationAbi, Erc721KYHAbi }
