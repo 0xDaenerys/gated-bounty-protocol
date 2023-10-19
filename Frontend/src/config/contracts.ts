aadb0c0e450458c5b3040f7ea6f53fc5fbf2e888
gg <gracecrypto03@gmail.com>
2023-10-19 19:54:42 +0530

    contract address config added

--- Frontend/src/config/contracts.ts
+++ Frontend/src/config/contracts.ts
@@ -1,22 +1,24 @@
 export const CONTRACTS = {
-    5: {
-        bountyFactory: "0x22d7154ad3410d4832Ad5b370C3d28332BF0E776",
-        erc20Reputation: "0x6fEA1eaDEdA1C649E6C28c262B75552A0c70c9D3",
-        erc721KYH: "0x29f03F66721CdC3a9738C798b5C6060adeD4cf17"
-    },
-    80001: {
-        bountyFactory: "",
-        erc20Reputation: "",
-        erc721KYH: ""
-    },
-    534351: {
-        bountyFactory: "",
-        erc20Reputation: "",
-        erc721KYH: ""
-    },
-    5001: {
-        bountyFactory: "",
-        erc20Reputation: "",
-        erc721KYH: ""
-    }
+  5: {
+    bountyFactory: '0x9C96F9931244e91D5858414A57533356548aa8D5',
+    erc20Reputation: '0xef79c2Fc9CF73A0A928D1cbE78dbE185160771D6',
+    erc721KYH: '0x4cc2581D88cFeCaf7C192Cae437d6aE9125AEB4e',
+  },
+  80001: {
+    bountyFactory: '0x22d7154ad3410d4832Ad5b370C3d28332BF0E776',
+    erc20Reputation: '0x6fEA1eaDEdA1C649E6C28c262B75552A0c70c9D3',
+    erc721KYH: '0x29f03F66721CdC3a9738C798b5C6060adeD4cf17',
+  },
+  // scroll
+  534351: {
+    bountyFactory: '0x22d7154ad3410d4832Ad5b370C3d28332BF0E776',
+    erc20Reputation: '0x6fEA1eaDEdA1C649E6C28c262B75552A0c70c9D3',
+    erc721KYH: '0x29f03F66721CdC3a9738C798b5C6060adeD4cf17',
+  },
+  // mantle
+  5001: {
+    bountyFactory: '0x22d7154ad3410d4832Ad5b370C3d28332BF0E776',
+    erc20Reputation: '0x6fEA1eaDEdA1C649E6C28c262B75552A0c70c9D3',
+    erc721KYH: '0x29f03F66721CdC3a9738C798b5C6060adeD4cf17',
+  },
 }
